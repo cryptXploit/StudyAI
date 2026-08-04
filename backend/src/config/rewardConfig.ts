314f7e9bd0cebc436f7a6a3d1c3aae2f35b7dbc4
cryptXploit <omarsunny18@gmail.com>
2026-08-04 21:01:09 +0600

    update: Adsterra Smartlink URL

--- backend/src/config/rewardConfig.ts
+++ backend/src/config/rewardConfig.ts
@@ -2,5 +2,5 @@ export const REWARD_CONFIG = {
   MAX_DAILY_ADS: 3,
   TOKENS_PER_AD: 10,
   AD_TIMER_SECONDS: 15,
-  SMARTLINK_URL: 'https://www.effectivecpmnetwork.com/cageq5eax?key=5948288fb14feceb63ccc8c358ad882d'
+  SMARTLINK_URL: 'https://witnessednone.com/cageq5eax?key=5948288fb14feceb63ccc8c358ad882d'
 };
